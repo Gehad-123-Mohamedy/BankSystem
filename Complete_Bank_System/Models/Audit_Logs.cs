91fe7981d3299c7f402dbfe4552233d7f850b41a
Gehad-123-Mohamedy <147076881+Gehad-123-Mohamedy@users.noreply.github.com>
2025-03-02 15:47:20 +0200

    authorize

--- Complete_Bank_System/Models/Audit_Logs.cs
+++ Complete_Bank_System/Models/Audit_Logs.cs
@@ -13,10 +13,12 @@ namespace Complete_Bank_System.Models
         public string ActionType { get; set; }
         public DateTime ActionDate { get; set; }
         [Required]
+        
         public int USerId { get; set; }
         [Required, StringLength(50)]
         public string UserType { get; set; }
         [Required, StringLength(100)]
+        
         public String Desc { get; set; }
     }
 }
